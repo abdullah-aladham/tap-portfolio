17d194c539d74c0cb352f5ec98fb843d2ee2ece1
abdullah-aladham <abdullah.aladham@gmail.com>
2024-01-19 19:10:56 +0200

    doing question 2

---
+++ Question-2/question2.js
@@ -0,0 +1,5 @@
+function SumFactorIndexes(nums, target) {}
+
+console.log(SumFactorIndexes([1, 2, 3, 4, 5], 5));
+console.log(SumFactorIndexes([1, 2, 3, 4, 5], 9));
+console.log(SumFactorIndexes([1, 2, 3, 4, 5], 7));
