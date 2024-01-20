48a662b469aa6d37e5bfba92d7ea7608f32a4a5e
abdullah-aladham <abdullah.aladham@gmail.com>
2024-01-21 00:46:07 +0200

    solved question 2 successfully

--- Question-2/question2.js
+++ Question-2/question2.js
@@ -1,5 +1,15 @@
-function SumFactorIndexes(nums, target) {}
-
-console.log(SumFactorIndexes([1, 2, 3, 4, 5], 5)); // indexes returned [1,2]
-console.log(SumFactorIndexes([1, 2, 3, 4, 5], 9));
-console.log(SumFactorIndexes([1, 2, 3, 4, 5], 7));
+function IndexSearch(nums, target) {
+  let i = 0;
+  while (nums.length != null) {
+    if (target === nums[i] + nums[i + 1]) {
+      return "first index = " + i + " ," + "Second index = " + (i + 1);
+    } else if (nums[i] + nums[i + 1] != target) {
+      i++;
+    } else {
+      break;
+    }
+  } //end of while loop
+  return -1; //indicates that the target indicies are not found
+}
+console.log(IndexSearch([1, 2, 3, 4], 5)); //first index 1, second index 2
+console.log(IndexSearch([1, 2, 3, 4], 7)); // first index 2 , second index 3
